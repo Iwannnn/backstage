6b4d9e59b064b7d19c978b04cbcf8b96e489bfbe
iwannnn <iwannnn1206@163.com>
2021-07-15 20:27:32 +0800

    init

---
+++ back-end/src/test/java/cn/iwannnn/backstage/BackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.iwannnn.backstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
