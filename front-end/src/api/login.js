c696fb93b7007013657dbcdf52d06318009383ab
iwannnn <iwannnn1206@163.com>
2021-07-18 15:30:44 +0800

    md5

--- front-end/src/api/login.js
+++ front-end/src/api/login.js
@@ -2,6 +2,7 @@ import request from "@/utils/request";
 
 // 登录方法
 export function login(username, password, uuid) {
+	// let md5_password = this.md5(password);
     const data = {
         username,
         password,
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,9 +1,3 @@
-/*
- * @Author: iwan
- * @Description:
- * @Date: 2021-07-15 17:17:38
- * @FilePath: \backstage\front-end\src\main.js
- */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
@@ -14,6 +8,7 @@ import router from "./router";
 
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
+
 
 Vue.use(ElementUI);
 
